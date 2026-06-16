1cda8a93a5530b335a41694dde1189cf7d1e42b2
nhannhanne234-ai <nhannhanne234@gmail.com>
2026-06-16 20:16:14 +0700

    Create cau7.py

---
+++ selection_sort/cau7.py
@@ -0,0 +1,11 @@
+def selection_sort_chars(a: list) -> list:
+    n = len(a)
+    for i in range(n - 1):
+        min_idx = i
+        for j in range(i + 1, n):
+            if a[j] < a[min_idx]:
+                min_idx = j
+        a[i], a[min_idx] = a[min_idx], a[i]
+    return a
+
+print(selection_sort_chars(['d', 'a', 'c', 'b']))

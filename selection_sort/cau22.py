a9c24ed37160aefa5f9456fed44ebd3eb09e9978
nhannhanne234-ai <nhannhanne234@gmail.com>
2026-06-16 20:16:52 +0700

    Create cau22.py

---
+++ selection_sort/cau22.py
@@ -0,0 +1,16 @@
+def stable_inplace_selection_sort(a):
+    n = len(a)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if a[j][0] < a[min_idx][0]: 
+                min_idx = j
+        key = a[min_idx]
+        while min_idx > i:
+            a[min_idx] = a[min_idx - 1]
+            min_idx -= 1
+        a[i] = key
+    return a
+
+a = [(2, 'a'), (2, 'b'), (1, 'c')]
+print(stable_inplace_selection_sort(a))
